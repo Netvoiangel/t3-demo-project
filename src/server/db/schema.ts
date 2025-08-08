486f5e68fc964c95ce957ec65d87c63efdf15119
Timur  w6itec6apel <w6itec6apel@Timurs-MacBook-Air.local>
2025-08-08 03:54:51 +0300

    chore: unsafe lint all files

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,13 +1,11 @@
 import { sql } from "drizzle-orm";
 import { sqliteTable } from "drizzle-orm/sqlite-core";
 
-export const events = sqliteTable('events', 
-	(d) => ({
-		id: d.integer("id", {mode: 'number'}).primaryKey({autoIncrement: true}),
-		title: d.text("title").notNull(),
-		description: d.text('description').notNull(),
-		date: d.integer('date', { mode: 'timestamp'}).notNull(),
-		location: d.text('location').notNull(),
-		createdAt: d.integer('createdAt', { mode: 'timestamp' }).default(sql`(unixepoch())`)
-	})
-)
+export const events = sqliteTable("events", (d) => ({
+  id: d.integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  title: d.text("title").notNull(),
+  description: d.text("description").notNull(),
+  date: d.integer("date", { mode: "timestamp" }).notNull(),
+  location: d.text("location").notNull(),
+  createdAt: d.integer("createdAt", { mode: "timestamp" }).default(sql`(unixepoch())`),
+}));
