0ba5ec5f8106478e21cc90f4dc5d3c09a00c9f07
Timur  w6itec6apel <w6itec6apel@Timurs-MacBook-Air.local>
2025-08-09 06:13:05 +0300

    feat: #15 added ui components from another time-project

---
+++ src/app/_components/events/event-list.js
@@ -0,0 +1,21 @@
+import EventItem from './event-item';
+import classes from './event-list.module.css';
+
+export default function EventList(props) {
+    const { items } = props;
+
+    return (
+        <ul className={classes.list}>
+            {items.map((item) => (
+                <EventItem
+                    key={item.id}
+                    title={item.title}
+                    location={item.location}
+                    date={item.date}
+                    image={item.image}
+                    id={item.id}
+                />
+            ))}
+        </ul>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,8 @@
-// import Link from "next/link";
-
-// import { LatestPost } from "~/app/_components/post";
-// import { HydrateClient, api } from "~/trpc/server";
-// import styles from "./index.module.css";
-
-// export default async function Home() {
-//   const hello = await api.post.hello({ text: "from tRPC" });
-
-//   void api.post.getLatest.prefetch();
-
-//   return (
-//     <HydrateClient>
-//       <main className={styles.main}>
-//         <div className={styles.container}>
-//           <h1 className={styles.title}>
-//             Create <span className={styles.pinkSpan}>T3</span> App
-//           </h1>
-//           <div className={styles.cardRow}>
-//             <Link
-//               className={styles.card}
-//               href="https://create.t3.gg/en/usage/first-steps"
-//               target="_blank"
-//             >
-//               <h3 className={styles.cardTitle}>First Steps →</h3>
-//               <div className={styles.cardText}>
-//                 Just the basics - Everything you need to know to set up your database and
-//                 authentication.
-//               </div>
-//             </Link>
-//             <Link
-//               className={styles.card}
-//               href="https://create.t3.gg/en/introduction"
-//               target="_blank"
-//             >
-//               <h3 className={styles.cardTitle}>Documentation →</h3>
-//               <div className={styles.cardText}>
-//                 Learn more about Create T3 App, the libraries it uses, and how to deploy it.
-//               </div>
-//             </Link>
-//           </div>
-//           <div className={styles.showcaseContainer}>
-//             <p className={styles.showcaseText}>
-//               {hello ? hello.greeting : "Loading tRPC query..."}
-//             </p>
-//           </div>
-
-//           <LatestPost />
-//         </div>
-//       </main>
-//     </HydrateClient>
-//   );
-// }
-
 import { drizzle } from "drizzle-orm/libsql";
+
+import { Fragment } from "react/jsx-runtime";
 import { events } from "~/server/db/schema";
+import EventList from "./_components/events/event-list";
 
 type events = {
   id: number;
@@ -64,22 +13,24 @@ type events = {
   createdAt: Date | null;
 }[];
 
-export default async function Home() {
+export default async function AllEventsPage() {
+  // const events = getAllEvents();
+
+
+  // function findEventsHandler(year, month) {
+  //     const fullpath = `/events/${year}/${month}`;
+
+  //     router.push(fullpath);
+  // }
+
   const db = drizzle(process.env.DATABASE_URL!);
 
   const allEvents: events = await db.select().from(events);
-  console.log(allEvents);
 
   return (
-    <div>
-      <h1>Events</h1>
-      <ul>
-        {allEvents.map((event) => (
-          <li key={event.id}>
-            {event.title}: {event.description}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Fragment>
+      {/* <EventsSearch onSearch={findEventsHandler} /> */}
+      <EventList items={allEvents} />
+    </Fragment>
   );
 }
