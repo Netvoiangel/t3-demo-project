6373b93019ab20c3d09b3ceb85f1d442ce68f1ba
Timur  w6itec6apel <w6itec6apel@Timurs-MacBook-Air.local>
2025-08-11 16:12:27 +0500

    feat: #17 edited file

---
+++ src/app/events/page.tsx
@@ -0,0 +1,31 @@
+import { Fragment } from "react/jsx-runtime";
+import { api } from "~/trpc/server";
+import EventList from "../_components/events/event-list";
+
+type events = {
+  id: number;
+  title: string;
+  description: string;
+  date: Date;
+  location: string;
+  createdAt: Date | null;
+}[];
+
+export default async function AllEventsPage() {
+  // const events = getAllEvents();
+
+  // function findEventsHandler(year, month) {
+  //     const fullpath = `/events/${year}/${month}`;
+
+  //     router.push(fullpath);
+  // }
+
+  const events: events = await api.event.getAllEvents();
+
+  return (
+    <Fragment>
+      {/* <EventsSearch onSearch={findEventsHandler} /> */}
+      <EventList items={events} />
+    </Fragment>
+  );
+}
