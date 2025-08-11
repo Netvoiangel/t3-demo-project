97868c2de269f98323133c3d70fde5bfdd1280cd
Timur  w6itec6apel <w6itec6apel@Timurs-MacBook-Air.local>
2025-08-11 16:19:03 +0500

    feat: #17 added components

--- src/app/_components/events/event-list.js
+++ src/app/_components/events/event-list.js
@@ -1,21 +1,21 @@
-import EventItem from './event-item';
-import classes from './event-list.module.css';
+import EventItem from "./event-item";
+import classes from "./event-list.module.css";
 
 export default function EventList(props) {
-    const { items } = props;
+  const { items } = props;
 
-    return (
-        <ul className={classes.list}>
-            {items.map((item) => (
-                <EventItem
-                    key={item.id}
-                    title={item.title}
-                    location={item.location}
-                    date={item.date}
-                    image={item.image}
-                    id={item.id}
-                />
-            ))}
-        </ul>
-    );
+  return (
+    <ul className={classes.list}>
+      {items.map((item) => (
+        <EventItem
+          key={item.id}
+          title={item.title}
+          location={item.location}
+          date={item.date}
+          image={item.image}
+          id={item.id}
+        />
+      ))}
+    </ul>
+  );
 }
