43ffe71e19efbdd4dbf64603ff6b5379301637f2
Timur  w6itec6apel <w6itec6apel@Timurs-MacBook-Air.local>
2025-08-18 21:26:55 +0300

    feat: #18 edited data fetching functions & added dummy images

--- drizzle/0000_tearful_wallflower.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `events` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`title` text NOT NULL,
-	`image` text NOT NULL,
-	`description` text NOT NULL,
-	`date` integer NOT NULL,
-	`location` text NOT NULL,
-	`createdAt` integer DEFAULT (unixepoch())
-);
