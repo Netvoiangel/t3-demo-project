9db69684a47eeae7b886b593bfd61bf087f8087f
Timur  w6itec6apel <w6itec6apel@Timurs-MacBook-Air.local>
2025-08-18 20:55:17 +0300

    feat: #18 added field image to events table & added new dummy data

--- drizzle/0000_tearful_wallflower.sql
+++ drizzle/0000_tearful_wallflower.sql
@@ -1,10 +1,9 @@
 CREATE TABLE `events` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`title` text NOT NULL,
+	`image` text NOT NULL,
 	`description` text NOT NULL,
 	`date` integer NOT NULL,
 	`location` text NOT NULL,
 	`createdAt` integer DEFAULT (unixepoch())
 );
---> statement-breakpoint
-DROP TABLE `t3-demo-project_post`;
